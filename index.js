1b321f4bb998f296831ae77bf54027f92806eaba
DKabucho <danielkabucho2004@gmail.com>
2023-06-14 12:29:43 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+const thankYouMessages = [];
+
+function writeCards(names,event) {
+    for(let i = 0; i < names.length; i++){
+       const messages = `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+       thankYouMessages.push(messages);
+    }
+    return thankYouMessages;
+}
+function countDown(number) {
+    let countDown = 10
+    while(countDown>=0) {
+        console.log(countDown--);
+    }
+}
